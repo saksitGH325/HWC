2a793572f839d64189ee13b7560d556d61863204
Dell <Dell@BaDLT>
2014-12-28 10:39:08 +0700

    first1 commit

--- src/main/java/com/psyrin/chapter2/HelloWorld.java
+++ src/main/java/com/psyrin/chapter2/HelloWorld.java
@@ -8,7 +8,7 @@ package com.psyrin.chapter2;
 public class HelloWorld {
     
         public static void main(String[] args) {
-        System.out.println("Helloworld");
+        System.out.println("Helloworld2");
                 
     }
             
